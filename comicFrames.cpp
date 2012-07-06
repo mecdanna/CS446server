e3ce031a240f23ba2f0dd85e6cb279fec60a6f5b
Mecdanna <mecdanna@gmail.com>
2012-07-05 22:10:57 -0400

    implemented comicFrames

--- comicFrames.cpp
+++ comicFrames.cpp
@@ -2,15 +2,19 @@
 
 using namespace std;
 
-ComicFrames::ComicFrames(int page, int width, int height) {
+ComicFrames::ComicFrames(uint page, uint width, uint height) : m_page(page), m_pageWidth(width), m_pageHeight(height) {
+	
 }
 
 void ComicFrames::append(const comicFrame& f) {
+	m_frames.push_back(f);
 }
 
-const comicFrame& ComicFrames::operator [](int idx) {
+const comicFrame& ComicFrames::operator [](uint idx) {
+	return m_frames[idx];
 }
 
 ComicFrames::~ComicFrames() {
+	m_frames.clear();
 }
 
--- comicFrames.h
+++ comicFrames.h
@@ -10,34 +10,36 @@
 
 #include <vector>
 
+typedef unsigned int uint;
+
 struct comicFrame {
 	comicFrame() {}
 
-	comicFrame(int x, int y, int w, int h, int label) : x(x), y(y), w(w), h(h), lbl(label) {}
+	comicFrame(uint x, uint y, uint w, uint h, uint label) : x(x), y(y), w(w), h(h), lbl(label) {}
 
-	int width() {return w;}
-	int height() {return h;}
-	int xPos() {return x;}
-	int yPos() {return y;}
-	int label() {return lbl;}
+	uint width() {return w;}
+	uint height() {return h;}
+	uint xPos() {return x;}
+	uint yPos() {return y;}
+	uint label() {return lbl;}
 
 private:
-	int x, y, w, h, lbl;
+	uint x, y, w, h, lbl;
 };
 
 class ComicFrames {
-	int m_page;
-	int m_pageWidth;
-	int m_pageHeight;
+	uint m_page;
+	uint m_pageWidth;
+	uint m_pageHeight;
 	std::vector<comicFrame> m_frames;
 public:
-	ComicFrames(int page = -1, int width = 0, int height = 0);
+	ComicFrames(uint page = -1, uint width = 0, uint height = 0);
 	~ComicFrames();
 
 	void append(const comicFrame &f);
-	const comicFrame& operator[](int idx);
-	int count() const {return m_frames.size();}
-	int pageNumber() const {return m_page;}
+	const comicFrame& operator[](uint idx);
+	uint count() const {return m_frames.size();}
+	uint pageNumber() const {return m_page;}
 };
 
 
