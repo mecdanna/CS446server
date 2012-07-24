dcb4854c05620a92c2245a01a644d7b807cc96df
Mecdanna <mecdanna@gmail.com>
2012-07-24 16:58:11 -0400

    Rewrote and bug tested, also commented

--- frameDetect.cpp
+++ frameDetect.cpp
@@ -1,13 +1,18 @@
 #include "frameDetect.h"
 #include "comicFrames.h"
+#include <iostream>
 
 using namespace std;
 
 /*
- * This uses -1 as a flag for an invalid answer, which is 2^32. Since there aren't any comics that have that many pixels,
- * let alone labels, this won't be a problem.
+ * This uses -1 as a flag for an invalid answer, which is 2^32-1 in an uint. Since there aren't any comics 
+ * that have that many pixels, let alone labels, this won't be a problem.
  */
 
+/*
+ * Initializes a new FrameDetect by binarizing the image, determining bcolour, and 
+ * adding white borders.
+ */
 FrameDetect::FrameDetect(AbstractImage<pixel>& page, uint pageNum) : rimg(page) {
 	bimg = rimg.binarize(228);
 
@@ -23,31 +28,40 @@ FrameDetect::FrameDetect(AbstractImage<pixel>& page, uint pageNum) : rimg(page) 
 
 FrameDetect::~FrameDetect() {}
 
-
+//Public wrapper
 ComicFrames FrameDetect::process() {
 	return process(1, 1, w, h-1);
 }
+
+/*
+ * Follows a contour (a line) and labels it differently
+ */
 void FrameDetect::contourTracking(LabelData& labelData, uint x, uint y, uint initialPos, uint lbl) {
 
-	uint c = initialPos;
+	uint d = initialPos;
 	Point s(x, y);
 	
 	bool atStart = false; //gotten back to initial position
-	bool nFollowsS = false; //ahead of initial position (circled around)
+	bool tFollowsS = false; //ahead of initial position (circled around)
 	
-	Point n = tracer(labelData, x, y, c, lbl);
-	if (n.x < 0) { //isolated point
+	Point t = tracer(labelData, x, y, d, lbl);
+	if (t.x < 0) { //isolated point
 		return;
 	}
 	
-	labelData(n.x, n.y) = lbl;
-	x = n.x;
-	y = n.y;
+	labelData(t.x, t.y) = lbl;
+	x = t.x;
+	y = t.y;
 	
-	while(!atStart || !nFollowsS) {
-		c = (c + 6) % 8; //previous contour is d+4, next initial position is d+2
+	while(!atStart || !tFollowsS) {
+		d = (d + 6) % 8; //previous contour is d+4, next initial position is d+2
 		
-		const Point f(tracer(labelData, x, y, c, lbl));
+		Point f = tracer(labelData, x, y, d, lbl);
+		/*cout << "f.x: " << f.x << 
+			" f.y: " << f.y << 
+			" x, y: " << x << "," << y <<
+			" s.x: " << s.x << 
+			" s.y: " << s.y << endl; */
 		
 		if (f.x < 0) { //image boundary
 			return;
@@ -55,12 +69,10 @@ void FrameDetect::contourTracking(LabelData& labelData, uint x, uint y, uint ini
 		
 		if (f == s) {
 			atStart = true;
-		} else if (f == n) {
-			if (atStart) {
-				nFollowsS = true;
-			}
+		} else if (f == t) {
+			tFollowsS = atStart ? true : false;
 		} else {
-			atStart = nFollowsS = false;
+			atStart = tFollowsS = false;
 		}
 		
 		labelData(f.x, f.y) = lbl;
@@ -70,6 +82,9 @@ void FrameDetect::contourTracking(LabelData& labelData, uint x, uint y, uint ini
 	}
 }
 
+/*
+ * Traces the outside of the shape
+ */
 FrameDetect::Point FrameDetect::tracer(LabelData& labelData, uint x, uint y, uint pos, uint lbl) {
 	for (uint i = 7; i >= 0; --i) {
 		uint tx = x;
@@ -77,14 +92,12 @@ FrameDetect::Point FrameDetect::tracer(LabelData& labelData, uint x, uint y, uin
 		nextPoint(tx, ty, pos);
 		
 		if (tx > 0 && ty > 0 && tx < w && ty < h) {
-			const uint &l = labelData(tx,ty);
+			uint l = labelData(tx,ty);
 			
-			if (bimg(tx,ty) == ccolour && (l == 0 || l == lbl))
-			{
+			if (bimg(tx,ty) == ccolour && (l == 0 || l == lbl)) {
 				return Point(tx,ty);
 			}
-			if (bimg(tx,ty) == bcolour)
-			{
+			if (bimg(tx,ty) == bcolour) {
 				labelData(tx,ty) = -1;
 			}
 		}
@@ -94,6 +107,9 @@ FrameDetect::Point FrameDetect::tracer(LabelData& labelData, uint x, uint y, uin
 	return Point(-1, -1);
 }
 
+/*
+ * Finds a bounding box for each frame and adds it to a ComicFrames, which it then returns
+ */
 ComicFrames FrameDetect::frames(LabelData& labelData) const {
 	vector<uint> x1(label);
 	vector<uint> x2(label);
@@ -140,6 +156,10 @@ ComicFrames FrameDetect::frames(LabelData& labelData) const {
 	return comicFrames;
 }
 
+/*
+ * The backbone of the algorithm, goes through each pixel and 
+ * assigns a label depending on which frame it thinks it belongs to.
+ */
 ComicFrames FrameDetect::process(uint px, uint py, uint pw, uint ph) {
 	LabelData labelData(w, h); //allocates more than required
 	labelData.fill(0);
@@ -149,14 +169,14 @@ ComicFrames FrameDetect::process(uint px, uint py, uint pw, uint ph) {
 		for (uint x = px; x < px + pw; ++x) {
 			uint p = labelData(x,y);
 			
-			if (bimg(x,y) == ccolour) { //find contour pixel
-			//step 1
+			if (bimg(x,y) == ccolour) { //found contour pixel
 				if (p == 0 && bimg(x,y-1) == bcolour) {
+					//step 1
 					p = label;
 					contourTracking(labelData, x, y, 7, label++);
 					
-				} else if (labelData(x,y+1) == 0 && bimg(x,y+1) == bcolour) { //pixel is below unmarked pixel
-			//step 2
+				} else if (labelData(x,y+1) == 0 && bimg(x,y+1) == bcolour) { //pixel below is unmarked
+					//step 2
 					if (p == 0) {
 						p = labelData(x-1, y); //copy label
 					}
@@ -172,18 +192,27 @@ ComicFrames FrameDetect::process(uint px, uint py, uint pw, uint ph) {
 	return frames(labelData);
 }
 
+/*
+ * Converts the boundary of the page into a bounding box of bcolour
+ * so it has a start point & we can return the entire image as a frame
+ * for the worst case of not finding any frames on a page.
+ */
 void FrameDetect::addWhiteBorders() {
-	for(uint x = 0; x < w-1; ++x) {
+	for(uint x = 0; x < w; ++x) {
 		bimg(x,0) = bimg(x,h-1) = bcolour;
 	}
 	
-	for(uint y = 0; y < h-1; ++y) {
+	for(uint y = 1; y < h-1; ++y) {
 		bimg(0,y) = bimg(w-1,y) = bcolour;
 	}
 }
 
+/*
+ * Background colour == the most common colour, which is important
+ * only because it is /not/ the contour colour.
+ */
 byte FrameDetect::determineBackground(AbstractImage<byte>& img) {
-	uint stripWidth = w/100;
+	uint stripWidth = w/100; //small optimization, no real need to scan entire image
 	
 	uint black = 0;
 	uint white = 0;
--- frameDetect.h
+++ frameDetect.h
@@ -13,12 +13,22 @@
 
 typedef AbstractImage<uint> LabelData;
 
+/*
+* This class detects all of the frames in a single page and returns them packaged.
+* Very roughly, the algorithm works as follows:
+* We binarize the image into background colour & a contour colour.
+* We create a labelData bitmap, which tells you which pixel belongs to which frame.
+* We then start filling it out by equating it to the label of the pixel that preceded it
+* until we run  into an unhandled ccolour pixel, which we track and trace around, labelling them
+* differently, before going back to filling the labels. Afterwards, we find bounding boxes for
+* the labels, and return them as the frames.
+*/
 class FrameDetect {
 	AbstractImage<pixel>& rimg;
 	AbstractImage<byte> bimg;
 	int page;
-	byte ccolour; //contour colour
-	byte bcolour; //background colour
+	byte ccolour; //contour colour (non-dominant binarized colour)
+	byte bcolour; //background colour (dominant binarized colour)
 	uint label;
 	uint w, h;
 protected:
@@ -29,9 +39,12 @@ protected:
 		bool operator==(const Point& p) const { return x == p.x && y == p.y; }
     };
 
+	/*
+	 * used to find the next point while tracing a contour.Traces around a hexagon
+	 * with an initial jump point (0) not being on the actual trace. 
+	 */
     inline static bool nextPoint(uint& x, uint& y, uint pos) {
-		switch (pos)
-		{
+		switch (pos) {
 			case 0: ++x; break;
 			case 1: ++x; ++y; break;
 			case 2: ++y; break;
@@ -40,24 +53,6 @@ protected:
 			case 5: --x; --y; break;
 			case 6: --y; break;
 			case 7: ++x; --y; break;
-			default:
-					return false; break;
-
-			return true;
-		}
-	}
-
-    inline bool nextPoint(uint& offset, uint pos) {
-		switch (pos)
-		{
-			case 0: ++offset; break;
-			case 1: offset += 1 + w; break;
-			case 2: offset += w; break;
-			case 3: offset += w-1; break;
-			case 4: --offset; break;
-			case 5: offset -= w+1; break;
-			case 6: offset -= w; break;
-			case 7: offset += w-1; break;
 			default:
 					return false; break;
 
